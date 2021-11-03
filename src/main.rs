a99a345f6d984b4f65b08593ff8e0dcc45143f85
Thibault Polge <thibault@thb.lt>
2021-11-03 15:12:24 +0100

    Add copyright headers, fix years

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use err_derive::Error;
-use std::env;
-use std::io;
-use std::process;
+//                                      Hey, EMACS: -*- nroff -*-
+// This file is part of pgp-words.
+// Copyright (c) 2020 Thibault Polge <thibault@thb.lt>.
+//
+// pgp-words is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// pgp-words is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use err_derive::Error; use std::env; use std::io; use std::process;
 
 const WORDS: &'static [(&'static str, &'static str)] = &[
     ("aardvark", "adroitness"),
